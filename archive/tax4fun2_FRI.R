8e9930e7593624fc67ce2182e9aaf5bfcae4ad1a
aliciamrich <aliciamrich@gmail.com>
2025-03-02 14:14:11 -0600

    files in queue to push

---
+++ archive/tax4fun2_FRI.R
@@ -0,0 +1,3 @@
+keg.main$cal_tax4fun2_FRI()
+keg.culi$cal_tax4fun2_FRI()
+keg.warb$cal_tax4fun2_FRI()
--- batch_scripts/tax4fun2_FRI.R
+++
@@ -1,3 +0,0 @@
-keg.main$cal_tax4fun2_FRI()
-keg.culi$cal_tax4fun2_FRI()
-keg.warb$cal_tax4fun2_FRI()
