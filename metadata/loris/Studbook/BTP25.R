2ccfd1b47833269bd0a757c397fc7fa41a42063b
aliciamrich <aliciamrich@gmail.com>
2025-03-13 12:15:50 -0500

    loris population data

---
+++ metadata/loris/Studbook/BTP25.R
@@ -0,0 +1,485 @@
+
+"AKRON" = list(
+  "Institution" = "Akron Zoological Park",
+  "Address"     = "500 Edgewood Avenue, Akron, Ohio 44307, United States",
+  "Lorises"     = list(
+    "2690" = list(
+      "Age"         = 5,
+      "Disposition" = "HOLD",
+      "Location"    = "AKRON",
+      "Breeding"    = "DO NOT BREED"
+      )
+    )
+  )
+
+"BLOOMINGT" = list(
+  "Institution" = "Miller Park Zoo",
+  "Address"     = "1020 S Morris Avenue, Bloomington, Illinois 61701, United States",
+  "Lorises"     = list(
+    "2622" = list(
+        "Age"         = "14",
+        "Disposition" = "RECEIVE FROM",
+        "Location"    = "CINCINNAT",
+        "Breeding"    = "BREED WITH",
+        "MateID"      = "2661"
+    ),
+    "2661" = list(
+      "Age"         = "9",
+      "Disposition" = "HOLD",
+      "Location"    = "BLOOMINGT",
+      "Breeding"    = "BREED WITH",
+      "MateID"      = "2622"
+    ),
+    "2682" = list(
+      "Age"         = "7",
+      "Disposition" = "SEND TO",
+      "Location"    = "NY BRONX",
+      "Breeding"    = "BREED WITH",
+      "MateID"      = "2708"
+    )
+  )
+)
+
+"CHICAGOLP" = list(
+  "Institution"   = "Lincoln Park Zoological Gardens",
+  "Address"       = "2001 N Clark Street, Chicago, Illinois 60614, United States",
+  "Facility Note" = "The SSP Coordinator is aware of the health monitoring of 2707, please reach out if you are interested in breeding in the future.",
+  "Lorises"       = list(
+    "2637" = list(
+      "Age"         = "13",
+      "Disposition" = "HOLD",
+      "Location"    = "CHICAGOLP",
+      "Breeding"    = "DO NOT BREED",
+      "Note"        = "Excluded - Behavior"
+      ),
+    "2646" = list(
+      "Age"         = "11",
+      "Disposition" = "HOLD",
+      "Location"    = "CHICAGOLP",
+      "Breeding"    = "DO NOT BREED"
+      ),
+    "2707" = list(
+      "Age"         = "4",
+      "Disposition" = "HOLD",
+      "Location"    = "CHICAGOLP",
+      "Breeding"    = "DO NOT BREED"
+    )
+    )
+  )
+    
+"CINCINNAT" = list(
+  "Institution"   = "Cincinnati Zoo & Botanical Garden",
+  "Address"       = "3400 Vine Street, Cincinnati, Ohio 45220, United States",
+  "Facility Note" = "A breeding option for male 2738 will be identified as more space and individuals become available.",
+  "Lorises"       = list(
+    "2622" = list(
+      "Age"         = "14",
+      "Disposition" = "SEND TO",
+      "Location"    = "BLOOMINGT",
+      "Breeding"    = "BREED WITH",
+      "MateID"      = "2661"
+    ),
+    "2630" = list(
+      "Age"         = "14",
+      "Disposition" = "HOLD",
+      "Location"    = "CINCINNAT",
+      "Breeding"    = "DO NOT BREED",
+      "Note"        = "Excluded - Behavior"
+    ),
+    "2738" = list(
+      "Age"         = "0",
+      "Disposition" = "HOLD",
+      "Location"    = "CINCINNAT",
+      "Breeding"    = "DO NOT BREED"
+    )
+  )
+)
+
+"CLEVELAND" = list(
+  "Institution"   = "Cleveland Metroparks Zoo",
+  "Address"       = "3900 Wildlife Way, Cleveland, Ohio 44109, United States",
+  "Lorises"       = list(
+    "2667" = list(
+      "Age"         = "8",
+      "Disposition" = "HOLD",
+      "Location"    = "CLEVELAND",
+      "Breeding"    = "DO NOT BREED"
+    ),
+    "2714" = list(
+      "Age"         = "3",
+      "Disposition" = "HOLD",
+      "Location"    = "CLEVELAND",
+      "Breeding"    = "DO NOT BREED"
+    ),
+    "2727" = list(
+      "Age"         = "1",
+      "Disposition" = "HOLD",
+      "Location"    = "CLEVELAND",
+      "Breeding"    = "DO NOT BREED"
+    )
+  )
+)
+
+"COLUMBUS" = list(
+  "Institution"   = "Columbus Zoo and Aquarium",
+  "Address"       = "PO Box 400, Powell, Ohio 43065, United States",
+  "Lorises"       = list(
+    "2639" = list(
+      "Age"         = "13",
+      "Disposition" = "HOLD",
+      "Location"    = "COLUMBUS",
+      "Breeding"    = "DO NOT BREED",
+      "Note"        = "Died during comment period"
+    ),
+    "2642" = list(
+      "Age"         = "13",
+      "Disposition" = "HOLD",
+      "Location"    = "COLUMBUS",
+      "Breeding"    = "BREED WITH",
+      "MateID"      = "2689"
+    ),
+    "2689" = list(
+      "Age"         = "5",
+      "Disposition" = "HOLD",
+      "Location"    = "COLUMBUS",
+      "Breeding"    = "BREED WITH",
+      "MateID"      = "2642"
+    )
+  )
+)
+
+"DULUTH" = list(
+  "Institution"   = "Lake Superior Zoological Gardens",
+  "Address"       = "7210 Fremont Street, Duluth, Minnesota 55807, United States",
+  "Lorises"       = list(
+    "2679" = list(
+      "Age"         = "7",
+      "Disposition" = "HOLD",
+      "Location"    = "DULUTH",
+      "Breeding"    = "BREED WITH",
+      "MateID"      = "2685"
+    ),
+    "2685" = list(
+      "Age"         = "7",
+      "Disposition" = "HOLD",
+      "Location"    = "DULUTH",
+      "Breeding"    = "BREED WITH",
+      "MateID"      = "2679"
+    )
+  )
+)
+
+"EL PASO" = list(
+  "Institution"   = "El Paso Zoo",
+  "Address"       = "4001 E Paisano Drive, El Paso, Texas 79905, United States",
+  "Facility Note" = "Please reach out to the SSP Coordinator when you are ready to receive new individuals.",
+  "Lorises"       = list(
+    "2504" = list(
+      "Age"         = "17",
+      "Disposition" = "HOLD",
+      "Location"    = "EL PASO",
+      "Breeding"    = "DO NOT BREED",
+      "Note"        = "Excluded - Age"
+    )
+  )
+)
+
+"GARDENCTY" = list(
+  "Institution"   = "Lee Richardson Zoo",
+  "Address"       = "312 Finnup Drive, Garden City, Kansas 67846, United States",
+  "Lorises"       = list(
+    "2666" = list(
+      "Age"         = "8",
+      "Disposition" = "HOLD",
+      "Location"    = "GARDENCTY",
+      "Breeding"    = "BREED WITH",
+      "MateID"      = "2671"
+    ),
+    "2671" = list(
+      "Age"         = "8",
+      "Disposition" = "HOLD",
+      "Location"    = "GARDENCTY",
+      "Breeding"    = "BREED WITH",
+      "MateID"      = "2666"
+    )
+  )
+)
+
+
+
+LITTLEROC
+Little Rock Zoological Gardens
+One Jonesboro Drive, Little Rock, Arkansas 72205, United States
+
+Facility Note: The SSP Coordinator is aware of your request to place the offspring and will work to fulfill this request as more space becomes available.
+
+2634
+14
+HOLD
+LITTLEROC
+DO NOT BREED
+Died during comment period
+
+2654
+10
+HOLD
+LITTLEROC
+DO NOT BREED
+
+
+2729
+1
+HOLD
+LITTLEROC
+DO NOT BREED
+
+
+2730
+1
+HOLD
+LITTLEROC
+DO NOT BREED
+
+
+
+MEMPHIS
+Memphis Zoological Garden & Aquarium
+2000 Prentiss Place, Memphis, Tennessee 38112, United States
+
+2673
+8
+HOLD
+MEMPHIS
+BREED WITH
+2718
+
+2718
+2
+HOLD
+MEMPHIS
+BREED WITH
+2673
+
+2732
+1
+HOLD
+MEMPHIS
+DO NOT BREED
+
+
+
+
+
+MILWAUKEE
+Milwaukee County Zoological Gardens
+10001 W Bluemound Road, Milwaukee, Wisconsin 53226, United States
+
+2626
+14
+HOLD
+MILWAUKEE
+DO NOT BREED
+
+Excluded - Behavior
+2631
+14
+HOLD
+MILWAUKEE
+DO NOT BREED
+
+
+MOODY
+Aquarium at Moody Gardens
+1 Hope Boulevard, Galveston, Texas 77554, United States
+
+2649
+12
+HOLD
+MOODY
+BREED WITH
+2659
+
+2659
+9
+HOLD
+MOODY
+BREED WITH
+2649
+
+2719
+2
+HOLD
+MOODY
+BREED WITH
+2723
+
+2723
+1
+HOLD
+MOODY
+BREED WITH
+2719
+
+
+
+
+NY BRONX
+Bronx Zoo/Wildlife Conservation Society
+2300 Southern Boulevard, Bronx, New York 10460, United States
+
+2623
+14
+HOLD
+NY BRONX
+BREED WITH
+2640
+
+2640
+12
+HOLD
+NY BRONX
+BREED WITH
+2623
+
+2682
+7
+RECEIVE FROM
+BLOOMINGT
+BREED WITH
+2708
+
+2708
+4
+RECEIVE FROM
+NZP-WASH
+BREED WITH
+2682
+
+
+
+
+NZP-WASH
+Smithsonian National Zoological Park
+3001 Connecticut Avenue NW, Washington, DC 20008, United States
+
+2708
+4
+SEND TO
+NY BRONX
+BREED WITH
+2682
+
+2715
+3
+HOLD
+NZP-WASH
+DO NOT BREED
+
+
+2735
+0
+HOLD
+NZP-WASH
+DO NOT BREED
+
+
+2736
+0
+HOLD
+NZP-WASH
+DO NOT BREED
+
+
+OMAHA
+Omaha Henry Doorly Zoo
+3701 S 10th Street, Omaha, Nebraska 68107, United States
+
+2652
+11
+HOLD
+OMAHA
+BREED WITH
+2677
+
+2677
+7
+HOLD
+OMAHA
+BREED WITH
+2652
+
+PHILADELP
+Philadelphia Zoo
+3400 W Girard Avenue, Philadelphia, Pennsylvania 19104, United States
+
+2617
+14
+HOLD
+PHILADELP
+DO NOT BREED
+
+Excluded - Age/No breeding success
+2676
+8
+HOLD
+PHILADELP
+BREED WITH
+2713
+
+2713
+3
+RECEIVE FROM
+W ORANGE
+BREED WITH
+2676
+
+
+
+
+PUEBLO
+Pueblo Zoo
+3455 Nuckolls Avenue, Pueblo, Colorado 81005, United States
+
+Facility Note: The SSP Coordinator acknowledges the lower likelihood of this pair breeding due to the female’s age, however, please do not prevent breeding.
+2614
+15
+HOLD
+PUEBLO
+BREED WITH
+2653
+
+2653
+10
+HOLD
+PUEBLO
+BREED WITH
+2614
+
+
+
+
+W ORANGE
+Turtle Back Zoo
+560 Northfield Avenue, West Orange, New Jersey 7052, United States
+
+2641
+12
+HOLD
+W ORANGE
+BREED WITH
+2717
+
+2713
+3
+SEND TO
+PHILADELP
+BREED WITH
+2676
+
+2717
+---
+HOLD
+W ORANGE
+BREED WITH
+2641
+
