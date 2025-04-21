45cdd3c6846b2abb08dcea131ab9eb4dee9fcc62
aliciamrich <aliciamrich@gmail.com>
2025-04-21 15:58:50 -0500

    marm tables added

---
+++ metadata/marmoset/crc_diets.R
@@ -0,0 +1,33 @@
+marm_foods <- list(
+  Biscuit = list(
+    "Zoopreem\u00AE Marmoset Diet",
+    "Commercially formulated diet made up of wheat, soybean, egg, alfalfa, and various vitamins",
+    1,
+    "slice"
+  ),
+  Gel = list(
+    "Mazuri\u00AE Callitrichid Gel Diet",
+    "Commercially formulated diet made up of soybean, corn, wheat, gelatin, and various vitamins",
+    1,
+    "slice"
+  ),
+  Apple = list(
+    "Apple",
+    "Cut into 1-cm^2 cubes",
+    4,
+    "pieces"
+  ),
+  SweetPotato = list(
+    "Sweet Potato",
+    "Cut into 1-cm^2 cubes",
+    4,
+    "pieces"
+  ),
+  Eggs = list(
+    "Eggs",
+    "Scrambled and microwave-cooked",
+    2,
+    "teaspoons"
+  )
+) %>%
+  list_flatten(name_spec = "")
